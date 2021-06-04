95f52c7a954a5cd3dda9ab78a1b97be68f6ab48a
Chris Klemz <crklemz@gmail.com>
2021-06-04 10:08:53 -0500

    created POST route

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,21 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+router.post('/', (req, res) => {
+    const listItem = req.body;
+    const sqlText = `INSERT INTO list (item, quantity, unit)
+                    VALUES ($1, $2, $3)`;
+
+    pool.query(sqlText, [listItem.item, listItem.quantity, listItem.unit])
+    .then((result) => {
+        console.log('added listItem to the database. list item is -->', listItem );
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log(`error making database query ${sqlText} in POST`, error);
+        res.sendStatus(500);
+    })
+})
+
 
 module.exports = router;
