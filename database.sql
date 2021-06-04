79ade862786f6c3354b03bbc4937a98b52246233
Chris Klemz <crklemz@gmail.com>
2021-06-04 09:49:39 -0500

    database created

--- database.sql
+++ database.sql
@@ -1,2 +1,17 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+--Database is called group-shopping-list
+
+CREATE TABLE list (
+	id SERIAL PRIMARY KEY,
+	item varchar(80),
+	quantity numeric not null,
+	unit varchar(20)
+);
+
+INSERT INTO "list" 
+	("item", "quantity", "unit") 
+VALUES 
+	('Apple', '35', 'lbs'), ('Bananas', '.5', 'bunch')
+	;
