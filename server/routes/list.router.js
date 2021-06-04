2d7bd901f2c912df040d7b21700c29c0b36ae6cd
Alex Kim <alexkim251@yahoo.com>
2021-06-04 10:07:01 -0500

    added Get route to list.router.js and also updated database in pool.js

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,18 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+//Get route
+router.get('/', (req, res) => {
+    const sqlText = `SELECT * FROM list`;
+    pool.query(sqlText)
+    .then((result) => {
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
+});
+
 
 module.exports = router;
